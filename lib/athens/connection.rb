1af043025e5d433ddead8b799e694f7217d63018
Chris Schulte <chris@oceanbreezesoftware.com>
2019-02-11 11:10:01 -0800

    Added code to map data to ruby types, return query results as array or hash

--- lib/athens/connection.rb
+++ lib/athens/connection.rb
@@ -1,3 +1,5 @@
+require 'bigdecimal'
+
 module Athens
   class Connection
     attr_reader :database_name
@@ -5,6 +7,9 @@ module Athens
     def initialize(database_name)
       @database_name = database_name
       @client = Aws::Athena::Client.new
+
+      version = RUBY_VERSION.split('.').map {|v| v.to_i}
+      @decimal_without_new = (version[0] >= 2 && version[1] >= 5)
     end
 
     def execute(query)
@@ -43,6 +48,43 @@ module Athens
       return query_result
     end
 
+    def execute_to_array(query, header_row:true)
+      result = execute(query)
+
+      metadata = result.result_set.result_set_metadata
+      rows = result.result_set.rows
+
+      array = []
+
+      first_row = rows.shift
+
+      if header_row
+        array << first_row.data.map {|col| col.var_char_value}
+      end
+
+      rows.each do |row|
+        array << map_types(metadata, row)
+      end
+
+      return array
+    end
+
+    def execute_to_hash(query)
+      rows = execute_to_array(query, header_row: true)
+      headers = rows.shift
+
+      array = []
+      rows.each do |row|
+        map = {}
+        headers.each_with_index do |header, index|
+          map[header] = row[index]
+        end
+        array << map
+      end
+
+      return array
+    end
+
     private
 
       def context 
@@ -56,6 +98,40 @@ module Athens
             encryption_option: "SSE_S3"
           }
         )
+      end
+
+      def map_types(metadata, row)
+        mapped = []
+
+        metadata.column_info.each_with_index do |col, index|
+          data = row.data[index].var_char_value
+
+          case col.type
+          when 'tinyint', 'smallint', 'int', 'integer', 'bigint'
+            mapped << data.to_i
+          when 'timestamp'
+            mapped << Time.parse(data)
+          when 'varchar'
+            mapped << data
+          when 'float', 'double'
+            mapped << data.to_f
+          when 'decimal'
+            if @decimal_without_new
+              mapped << BigDecimal(data)
+            else
+              mapped << BigDecimal.new(data)
+            end
+          when 'date'
+            mapped << Date.parse(data)
+          when 'boolean'
+            mapped << (data == "true")
+          else
+            puts "WARNING: Unsupported type: #{col.type}, defaulting to string"
+            mapped << data
+          end
+        end
+
+        return mapped
       end    
   end
 end
