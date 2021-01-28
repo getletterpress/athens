196fe91d2a1e48882643c2e821af9f755988e1f6
oisin <oi.sin@nis.io>
2021-01-28 10:52:41 +0000

    Added :aws_profile option

--- lib/athens/configuration.rb
+++ lib/athens/configuration.rb
@@ -1,15 +1,17 @@
 module Athens
   class Configuration
-    attr_accessor :aws_access_key, 
+    attr_accessor :aws_access_key,
                   :aws_secret_key,
                   :aws_region,
-                  :output_location
+                  :output_location,
+                  :aws_profile
 
     def initialize
       @aws_access_key = nil
       @aws_secret_key = nil
       @aws_region = nil
       @output_location = nil
+      @aws_profile = nil
     end
   end
 end
